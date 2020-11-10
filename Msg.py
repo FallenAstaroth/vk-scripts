b84b9a1da59fb2d14db71240526a0f49e9949cd5
Wolf In Chains <47662957+WolfInChains@users.noreply.github.com>
2020-11-10 13:16:48 +0200

    Update Msg.py

--- Msg.py
+++ Msg.py
@@ -7,12 +7,14 @@ vk = vk_session.get_api()
 
 index = 0
 
+print("\nЗапуск процесса накрутки...")
+print("Обработка сообщений...\n")
+
 while True:
     try:
         for msg in range(10):
             msgs = vk.execute(code='var offset =' + f'{index}' + ',chats=API.messages.getConversations({count:200,offset:offset}).items,i=0;while(i<chats.length){if(!chats[i].conversation.is_marked_unread){API.messages.markAsUnreadConversation({peer_id:chats[i].conversation.peer.id});}i=i+1;}return 1;')
         index += 100
-        print('Сообщения накручены')
-        print('Возобновление процесса накрутки')
+        print(f'{index} сообщений обработано')
     except Exception as e:
         print(repr(e))
