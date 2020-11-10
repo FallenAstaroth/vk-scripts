7419a3292f8ca603403e603ef7489296d51e75c8
Wolf In Chains <47662957+WolfInChains@users.noreply.github.com>
2020-11-10 12:54:09 +0200

    Update Msg.py

--- Msg.py
+++ Msg.py
@@ -5,10 +5,10 @@ token = input('Введите токен: ')
 vk_session = vk_api.VkApi(token=token)
 vk = vk_session.get_api()
 
+index = 0
 
 while True:
     try:
-        index = 0
         for msg in range(10):
             msgs = vk.execute(code='var offset =' + f'{index}' + ',chats=API.messages.getConversations({count:200,offset:offset}).items,i=0;while(i<chats.length){if(!chats[i].conversation.is_marked_unread){API.messages.markAsUnreadConversation({peer_id:chats[i].conversation.peer.id});}i=i+1;}return 1;')
         index += 100
