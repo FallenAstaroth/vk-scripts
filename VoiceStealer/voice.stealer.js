c7cfd9c13bd1a7203a0f0b452939453686eb87b8
Astaroth <47662957+FallenAstaroth@users.noreply.github.com>
2023-03-10 13:51:56 +0200

    Update voice.stealer.js

--- VoiceStealer/voice.stealer.js
+++ VoiceStealer/voice.stealer.js
@@ -9,7 +9,7 @@
 // @grant        GM.xmlHttpRequest
 // @run-at       document-end
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
-// @updateURL    https://raw.githubusercontent.com/FallenAstaroth/vk-scripts/master/VoiceStealer/voice.stealer.js
+// @updateURL    https://raw.githubusercontent.com/FallenAstaroth/scammers-scripts/master/VoiceStealer/voice.stealer.js
 // ==/UserScript==
 
 (async function() {
