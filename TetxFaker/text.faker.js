e3297e6db223bd3488ab4959ea66c0ac0774c278
Astaroth <47662957+FallenAstaroth@users.noreply.github.com>
2023-03-11 17:24:06 +0300

    Onload event added

--- TetxFaker/text.faker.js
+++ TetxFaker/text.faker.js
@@ -1,14 +1,13 @@
 // ==UserScript==
 // @name         Text Faker
 // @namespace    https://github.com/FallenAstaroth/
-// @version      1.0.0
+// @version      1.0.1
 // @description  Заменяет значения элементов до их рендеринга на странице
 // @author       FallenAstaroth
 // @match        https://github.com/FallenAstaroth/scammers-scripts
 // @icon         https://img.icons8.com/color/512/vk-circled.png
 // @grant        none
 // @run-at       document-start
-// @updateURL    https://raw.githubusercontent.com/FallenAstaroth/scammers-scripts/master/TextFaker/text.faker.js
 // ==/UserScript==
 
 (function() {
@@ -21,9 +20,11 @@
         }
     ]
 
-    elements.forEach((element) => {
-        replaceTextOnInsertion(element.selector, element.text);
-    })
+    window.onload = function() {
+        elements.forEach((element) => {
+            replaceTextOnInsertion(element.selector, element.text);
+        })
+    }
 
     function replaceTextOnInsertion(selector, newText) {
         const observer = new MutationObserver((mutationsList, observer) => {
